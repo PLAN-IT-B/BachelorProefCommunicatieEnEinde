4dd339784cb80bc80d9e7ed304a3ed324aa7c9bf
Henri Vandeputte <Vandeputtehenri@gmail.com>
2022-05-18 13:58:19 +0200

    UV-slot samenwerking met buffer

--- Documentatie UV-slot/UV_Slot - code/src/main.cpp
+++ Documentatie UV-slot/UV_Slot - code/src/main.cpp
@@ -25,7 +25,8 @@ void callback(char *topic, byte *message, unsigned int length);
 bool garbage_Ready = false;
 bool opgelost = false;
 bool blockKeypad = false;
-bool reset = false;
+bool Curr_energie = false;
+bool Prev_energie = false;
 
 //code slot
 int cinput[4];
@@ -65,6 +66,188 @@ unsigned long previous_time = 0;
 volatile short int start_pause = 0;
 int seconds, minutes;             // For switching between left & right part of the display(Even = left, Odd = right)
 
+//functies
+void setup_wifi();
+void setup_lcd();
+void tip();
+void reconnect();
+void UV_Enable();
+void callback(char *topic, byte *message, unsigned int length);
+
+
+
+void setup() {
+
+  pinMode(Relais_UV,OUTPUT); 
+  Serial.begin (115200);
+  digitalWrite(Relais_UV, LOW);
+  Serial.print("begin search");
+  Wire.begin();
+  byte count = 0;
+  for (byte i = 6; i < 120; i++)
+  {
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+      {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);  // maybe unneeded?
+      } // end of good response
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
+
+  //Wifi setup
+  setup_wifi();
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(callback);
+
+  //lcd setup
+  setup_lcd();
+
+}
+
+void loop() {
+  //Connectie checken en tesnoods reconnecten
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000)
+  {
+    lastMsg = now;
+  }
+  
+  char key = customKeypad.getKey();
+
+  if(Prev_energie == Curr_energie){
+
+  }else if(Curr_energie == true){
+    setup_lcd;
+    Prev_energie = Curr_energie;
+  }else if(Curr_energie == false){
+    lcd.clear();
+    lcd.noBacklight();
+  }
+
+  
+
+  
+   
+      //Als de knop wordt ingedrukt
+      if(key && blockKeypad == false && Curr_energie){
+
+        /*
+        Serial.print("De waarde uit het keypad is: ");
+        Serial.println(key);
+        */
+
+        //Als # (enter wordt ingedrukt)
+        if(key =='#'){    
+          if(c ==10 ){ //De positie is het laatste cijfer
+            opgelost = true; //Controleer of de code klopt
+            for(int i = 0;i<4;i++){
+              if(code[i]!=cinput[i]){
+                opgelost = false;
+                lcd.setCursor(6,1);
+                lcd.print("____");
+                c = 6;
+              }
+            }
+            if (garbage_Ready==false) {
+              tip();
+            }
+            if(opgelost){
+              UV_Enable();   
+              blockKeypad = true;
+            }
+
+            
+          }
+        }     
+        else if(key == '*'){ //Als * (terug) wordt ingevuld
+            //Ga 1 terug, vervang het getal door _ en vervang de code door 0 (standaard getal in rij)
+          if(c>6){
+            c--;
+            lcd.setCursor(c,1);
+            lcd.print("_");
+            cinput[c-6] = 0;
+          }
+        }
+        else{ //Als er iets anders (cijfer) wordt ingedrukt
+          if(c<10){ //Vul het getal in en schuif 1 plaats op.
+          lcd.setCursor(c,1);
+          lcd.print(key);
+          cinput[c-6]= key-'0';
+          lcd.setCursor(c,1);
+          c++;
+          lcd.display();
+          }    
+        }
+      }
+  
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp = "x";
+
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+    Serial.println();
+  }
+
+
+  //Als het een bericht is van de garbadge puzzel, zal het zijn voor de code te veranderen.
+  if (strcmp(topic,"garbage/eindcode") == 0) 
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      code[i] = message[i]-'0';
+    }
+    Serial.print("De code is veranderd naar ");
+      for (int i = 0; i < 4; i++)
+      {
+        Serial.print(code[i]);
+      }
+    Serial.println(); 
+    garbage_Ready = true;
+  }
+
+  //De statusvan de buffer
+  if (strcmp(topic,"garbage/eindcode") == 0) 
+  {
+    if(messageTemp == "Groen"){
+      Curr_energie = true;
+    }
+    else if(messageTemp == "Oranje"){
+      Curr_energie = true;
+    }
+    else if(messageTemp == "Rood"){
+      Curr_energie = false;
+    }
+  }
+
+  //Als het een bericht is om de escape room klaar te maken voor gebruik, gaan we resetten.
+   if (strcmp(topic,"controlpanel/reset") == 0) 
+  {
+    ESP.restart();
+  }
+}
 
 void setup_wifi()
 {
@@ -125,11 +308,13 @@ void reconnect()
     if (client.connect("UV-Slot_ESP"))
     {
       Serial.println("connected");
+      client.publish("controlpanel/status", "UV-slot Ready");
       // Subscribe
       // Vul hieronder in naar welke directories je gaat luisteren.
       //Voor de communicatie tussen de puzzels, check "Datacommunicatie.docx". (terug tevinden in dezelfde repository) 
       client.subscribe("controlpanel/reset");
       client.subscribe("garbage/eindcode");
+      client.subscribe("TrappenMaar/zone");
     }
     else
     {
@@ -160,166 +345,3 @@ void UV_Enable(){
   client.publish("controlpanel/UV-slot","UV licht staat geschakeld!");
 
 }
-
-void callback(char *topic, byte *message, unsigned int length)
-{
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp = "x";
-
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-    Serial.println();
-  }
-
-
-  //Als het een bericht is van de garbadge puzzel, zal het zijn voor de code te veranderen.
-  if (strcmp(topic,"garbage/eindcode") == 0) 
-  {
-    for (int i = 0; i < 4; i++)
-    {
-      code[i] = message[i]-'0';
-    }
-    Serial.print("De code is veranderd naar ");
-      for (int i = 0; i < 4; i++)
-      {
-        Serial.print(code[i]);
-      }
-    Serial.println(); 
-    garbage_Ready = true;
-  }
-
-  //Als het een bericht is om de escape room klaar te maken voor gebruik, gaan we resetten.
-   if (strcmp(topic,"controlpanel/reset") == 0) 
-  {
-    ESP.restart();
-  }
-
-
-
-}
-
-void setup() {
-
-  pinMode(Relais_UV,OUTPUT); 
-  Serial.begin (115200);
-  digitalWrite(Relais_UV, LOW);
-  Serial.print("begin search");
-  Wire.begin();
-  byte count = 0;
-  for (byte i = 6; i < 120; i++)
-  {
-    Wire.beginTransmission (i);
-    if (Wire.endTransmission () == 0)
-      {
-      Serial.print ("Found address: ");
-      Serial.print (i, DEC);
-      Serial.print (" (0x");
-      Serial.print (i, HEX);
-      Serial.println (")");
-      count++;
-      delay (1);  // maybe unneeded?
-      } // end of good response
-  } // end of for loop
-  Serial.println ("Done.");
-  Serial.print ("Found ");
-  Serial.print (count, DEC);
-  Serial.println (" device(s).");
-
-  //Wifi setup
-  setup_wifi();
-  client.setServer(MQTT_SERVER, MQTT_PORT);
-  client.setCallback(callback);
-
-  //lcd setup
-  setup_lcd();
-
-  reset = true;
-}
-
-void loop() {
-  //Connectie checken en tesnoods reconnecten
-  if (!client.connected())
-  {
-    reconnect();
-  }
-  client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 5000)
-  {
-    lastMsg = now;
-  }
-
-  if(reset){
-  //Als alle belangrijke setups gelukt zijn, gaan we dat zeggen tegen de centrale esp.
-  client.publish("controlpanel/status", "UV-slot Ready");
-  reset = false;
-  }
-
-  
-   char key = customKeypad.getKey();
-  
-    if(opgelost == true){ //Als de code klopt 
-      //gaat de deur open
-      UV_Enable();   
-      blockKeypad = true;
-
-    }
-    else{
-
-      //Als de knop wordt ingedrukt
-      if(key && blockKeypad == false){
-
-        /*
-        Serial.print("De waarde uit het keypad is: ");
-        Serial.println(key);
-        */
-
-        //Als # (enter wordt ingedrukt)
-        if(key =='#'){    
-          if(c ==10 ){ //De positie is het laatste cijfer
-            opgelost = true; //Controleer of de code klopt
-            for(int i = 0;i<4;i++){
-              if(code[i]!=cinput[i]){
-                opgelost = false;
-                lcd.setCursor(6,1);
-                lcd.print("____");
-                c = 6;
-              }
-            }
-            if (garbage_Ready==false) {
-              tip();
-            }
-            
-          }
-        }     
-
-       else if(key == '*'){ //Als * (terug) wordt ingevuld
-            //Ga 1 terug, vervang het getal door _ en vervang de code door 0 (standaard getal in rij)
-          if(c>6){
-            c--;
-            lcd.setCursor(c,1);
-            lcd.print("_");
-            cinput[c-6] = 0;
-          }
-          
-        }
-        else{ //Als er iets anders (cijfer) wordt ingedrukt
-          if(c<10){ //Vul het getal in en schuif 1 plaats op.
-          lcd.setCursor(c,1);
-          lcd.print(key);
-          cinput[c-6]= key-'0';
-          lcd.setCursor(c,1);
-          c++;
-          lcd.display();
-          }    
-        }
-      }
-   }
-  
-}
-
